9dabfe1a2adf38ce02c004d41ea2435640cbed80
Prismic <prismic@prismics-macbook-pro.home>
2020-12-30 11:07:08 +0100

    first-commit

---
+++ prismic-configuration.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  
+  // The repo name for your Prismic repository (found in the url of your repo: https://preview-test-multilang.prismic.io/)
+  prismicRepo: 'preview-test-multilang',
+
+  // Release ID to build (requires access)
+  releaseID: '',
+
+  // access token to access the release
+  accessToken: '',
+
+  // Language configuration in your Prismic repository: Settings / Translations & locals
+  defaultLanguage: 'en-us',
+  langs: ['en-us', 'fr-fr'],
+}
